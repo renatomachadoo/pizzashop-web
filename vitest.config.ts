058c21732e07da5311778544d295788312a9fef5
Renato Machado <renatomachado176@gmail.com>
2024-12-17 15:07:53 +0000

    order status and pagination unit tests

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+// vitest.config.ts
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+})
