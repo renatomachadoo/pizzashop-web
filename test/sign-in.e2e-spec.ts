1d68e235ae8335c4a19fc9d1ab61b5f71fc7be7e
Renato Machado <renatomachado176@gmail.com>
2024-12-18 15:00:21 +0000

    E2E Test sign-in

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = await page.getByText(
+    'Enviamos um link de autenticação para o seu email.',
+  )
+
+  expect(toast).toBeVisible()
+
+  await page.waitForTimeout(2000)
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-mail').fill('wrong@example.com')
+
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = await page.getByText('Credenciais inválidas.')
+
+  expect(toast).toBeVisible()
+
+  await page.waitForTimeout(2000)
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+
+  await page.waitForTimeout(2000)
+})
